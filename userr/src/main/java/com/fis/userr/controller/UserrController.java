d451b11e99fbf1bbe1eea1df922a44a31b988a3a
Saharsh <saharsharya2200@gmail.com>
2021-08-17 11:00:58 +0530

    implemented user microservices

--- userr/src/main/java/com/fis/userr/controller/UserrController.java
+++ userr/src/main/java/com/fis/userr/controller/UserrController.java
@@ -3,33 +3,33 @@ package com.fis.userr.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fis.userr.service.AppUserDetailsService;
+import com.fis.userr.bean.Userr;
+import com.fis.userr.repository.UserrRepository;
 
 
 @RestController
 @RequestMapping("/users")
 public class UserrController {
 
+//	@Autowired
+//	private AppUserDetailsService appUserDetailsService;
+//
+//	@GetMapping("/{username}")
+//	public UserDetails findByUsername(@PathVariable String username) {
+//		return appUserDetailsService.loadUserByUsername(username);
+//	}
+
 	@Autowired
-	private AppUserDetailsService appUserDetailsService;
+	private UserrRepository userRepository;
 
 	@GetMapping("/{username}")
-	public UserDetails findByUsername(@PathVariable String username) {
-		return appUserDetailsService.loadUserByUsername(username);
+	public Userr findByUsername(@PathVariable String username) {
+		return userRepository.findByUsername(username);
 	}
-
-//	@Autowired
-//	private UserRepository userRepository;
-
-//	@GetMapping("/{username}")
-//	public User findByUsername(@PathVariable String username) {
-//		return userRepository.findByUsername(username);
-//	}
 
 }
---
+++ userr/src/main/resources/create_user.sql
@@ -0,0 +1,32 @@
+create database ormlearn
+
+create table role (ro_id int, ro_name varchar(20))
+create table user (us_id int, us_username varchar(20), us_password varchar(20))
+
+create table user_role(ur_ro_id int, ur_us_id int)
+create table skill(sk_id int, sk_name varchar(20))
+
+
+create table department(dp_id int, dp_name varchar(20))
+
+create table employee_skill (es_em_id int, es_sk_id int)
+
+create table employee(em_id int, em_name varchar(20), em_salary double precision, em_permanent bit(1), em_date_of_birth datetime(6), em_dp_id int)
+
+
+use ormlearn;
+insert into employee values(2, "Random Name", 878787, 1, "1999/05/21 11:22:32.2222222",44)
+insert into department values(44, "Social science")
+insert into employee_skill values(100, 200)
+insert into employee_skill values(390, 400)
+insert into role values(1000, "developer");
+insert into role values(10001, "Tester");
+
+insert into skill values (2000, "HTML");
+insert into skill values (2001, "Java");
+
+insert into user values(5000, "user1", "random");
+insert into user values(5000, "user2", "random2");
+
+insert into user_roles values(90,91);
+insert into user_roles values(92,93);
